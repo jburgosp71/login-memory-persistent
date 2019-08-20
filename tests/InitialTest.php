4fb363bb0ba3ca4f77f143491716f5a908cb57b8
Javier Burgos <jburgosp71@gmail.com>
2019-08-20 23:55:07 +0200

    Initial settings

---
+++ tests/InitialTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\loginMemoryPersistent;
+
+class InitialTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInitialList()
+    {
+        $this->assertTrue(true);
+    }
+}
